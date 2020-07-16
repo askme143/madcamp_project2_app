5e98a70323a9096a56f2d5a1d2c8a7a78f875a0c
Yeongil Yoon <askme143@kaist.ac.kr>
2020-07-16 14:35:37 +0900

    Implement 3 tabs

--- app/src/main/java/com/example/madcampserverapp/MainActivity.java
+++ app/src/main/java/com/example/madcampserverapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.madcampserverapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.madcampserverapp.ui.contact.FragmentContact;
+import com.example.madcampserverapp.ui.gallery.FragmentGallery;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentContact fragmentContact;
+    private FragmentGallery fragmentGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentContact = new FragmentContact();
+        fragmentGallery = new FragmentGallery();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, fragmentContact).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView
+                .OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
+                switch (menuItem.getItemId()){
+                    case R.id.contact:{
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame_layout, fragmentContact).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.gallery:{
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frame_layout, fragmentGallery).commitAllowingStateLoss();
+                        return true;
+                    }
+                    case R.id.free:{
+                        return true;
+                    }
+                    default: return false;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/madcampserverapp/ui/contact/FragmentContact.java
@@ -0,0 +1,22 @@
+package com.example.madcampserverapp.ui.contact;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.madcampserverapp.R;
+
+public class FragmentContact extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contact, null);
+
+        return view;
+    }
+}
